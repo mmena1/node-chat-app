bded26d4998d3639cc2f4a99a8dd00b6ccccceb5
Martin Mena <martinmena@outlook.com>
2018-05-15 10:14:32 -0500

    First commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+const express = require('express');
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+// console.log(__dirname + '/../public');
+// console.log(publicPath);
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
